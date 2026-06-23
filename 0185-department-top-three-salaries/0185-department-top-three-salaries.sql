db6dc2cd02a251f3444519f24481865d796a2c03
Rutul D. Bhosale <68531667+Rutul117@users.noreply.github.com>
2026-06-23 11:31:41 +0530

    Time: 1478 ms (9.44%), Space: 0B (100%) - LeetHub

---
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -0,0 +1,12 @@
+WITH RankedSalaries AS (
+    SELECT 
+        e.name AS Employee,
+        e.salary AS Salary,
+        d.name AS Department,
+        DENSE_RANK() OVER (PARTITION BY e.departmentId ORDER BY e.salary DESC) AS DenseRank
+    FROM Employee e
+    JOIN Department d ON e.departmentId = d.id
+)
+SELECT Department, Employee, Salary
+FROM RankedSalaries
+WHERE DenseRank <= 3;
