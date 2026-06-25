1c061ef4a55037e535427c9ccf413bd8a6c64532
Rutul D. Bhosale <68531667+Rutul117@users.noreply.github.com>
2026-06-25 12:51:39 +0530

    Time: 13 ms (50.7%), Space: 19.5 MB (28.6%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        char_count = {}
+        
+        # Count characters in s
+        for char in s:
+            if char in char_count:
+                char_count[char] += 1
+            else:
+                char_count[char] = 1
+        
+        # Decrement counts using t
+        for char in t:
+            if char not in char_count:
+                return False
+            char_count[char] -= 1
+            if char_count[char] < 0:
+                return False
+        
+        return True   
