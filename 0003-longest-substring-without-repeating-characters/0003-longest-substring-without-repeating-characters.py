2430190587dcf469aa175fb2fc44523fc04fc06a
Rutul D. Bhosale <68531667+Rutul117@users.noreply.github.com>
2026-06-27 10:30:23 +0530

    Time: 20 ms (19.5%), Space: 19.2 MB (68.18%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -0,0 +1,12 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        longest = 0 # tracks maximum sibstring
+        l = 0 # start of Sliding window
+        counter : dict[str, int] = defaultdict(int)
+        for r in range(len(s)):
+            counter[s[r]] += 1
+            while counter[s[r]] > 1:
+                counter[s[l]] -= 1
+                l += 1
+            longest = max(longest, r - l + 1)
+        return longest
